783822f28b431590567f4803247ff17747bda063
Syed-Umair <syed.yousuff@anywhere.co>
2019-12-24 14:45:29 +0530

    updated website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="contactCard">
+        <div class="content">
+            <p>Syed Umair</p>
+            <p>Software Engineer</p>
+            <p>FULL Creative</p>
+            <p><a href="mailto:dev@ideasyed.com">dev@ideasyed.com</a></p>
+        </div>
     </div>
   );
 }
